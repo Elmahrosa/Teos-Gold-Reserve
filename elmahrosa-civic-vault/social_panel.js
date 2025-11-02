5db7011123c52319804f80adfaa88768531850c6
Ayman Seif <92527473+Aymanseif@users.noreply.github.com>
2025-11-03 00:17:28 +0200

    Create social_panel.js

---
+++ elmahrosa-civic-vault/social_panel.js
@@ -0,0 +1,6 @@
+export const socialLinks = {
+  telegram: "https://t.me/elmahrosapi",
+  twitter: "https://x.com/king_teos",
+  facebook: "https://facebook.com/elmahrosapi",
+  instagram: "https://instagram.com/elmahrosapi"
+};
